f30545bb7d059f6e04787125f0722360326960f2
rohit <rohit0311@outlook.com>
2020-10-20 10:02:26 +0530

    start page looks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import './App.css';
+import Button from './UI/Button';
 
-function App() {
-  return <div>hello guys</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="flex-row">
+          <div className="content">
+            <h3>Current Time</h3>
+            <h1>12:23</h1>
+          </div>
+          <div className="content">
+            <h3>Total streak Time</h3>
+            <h1>121:23</h1>
+          </div>
+        </div>
+        <div className="flex-row">
+          <div className="content">
+            <h3>Current Streak</h3>
+            <h1>5</h1>
+          </div>
+          <div className="content">
+            <h3>Maximum Streak</h3>
+            <h1>15</h1>
+          </div>
+        </div>
+        <div className="flex-row">
+          <Button text="stop" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
